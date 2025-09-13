b1f7586fe7f3a0c2c604208e20bc1b750b533d34
RAJ KUMAR <rk8210032@gmail.com>
2025-09-14 04:07:20 +0530

    added multiple components

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,18 +2,24 @@ import React, { useRef } from 'react';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import HeroSection from './landingPageSection/HeroSection';
+import FielsExperties from '../components/landingPageSection/FieldSection';
+import TechnicalSkills from '../components/landingPageSection/TechnicalSkills';
+import ConcactUs from '../components/landingPageSection/ContatcUsSection';
 
 const LandingPage = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow pt-[70px] sm:px-6 px-4">
+      <main className="flex-grow pt-[70px]">
 
         <div >
           <HeroSection />
+          <FielsExperties/>
+          <TechnicalSkills/>
+          <ConcactUs/>
         </div>
-        <div style={{ height: '1000px' }}></div>
+        {/* <div style={{ height: '1000px' }}></div> */}
       </main>
       <Footer />
     </div>
