dad893cace011b7c2c10c0cec44580ee254f9829
RAJ KUMAR <rk8210032@gmail.com>
2025-09-14 04:24:33 +0530

    increase the margin bottom for clen view

--- src/components/landingPageSection/FieldSection.jsx
+++ src/components/landingPageSection/FieldSection.jsx
@@ -32,7 +32,7 @@ const fields = [
 
 const FieldSection = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-full min-h-screen px-4 ">
+        <div className="flex flex-col items-center justify-center w-full min-h-screen px-4 pb-20">
             <h2 className=" font-main font-bold text-white text-4xl mb-12 tracking-wide">
                 Field Experties
             </h2>
