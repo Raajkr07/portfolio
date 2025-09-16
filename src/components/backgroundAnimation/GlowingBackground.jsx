a603ca823bf7409f59a0ab260309f18f6a827f4e
RAJ KUMAR <rk8210032@gmail.com>
2025-09-16 22:26:50 +0530

    reduce the radious as per herosection image

--- src/components/backgroundAnimation/GlowingBackground.jsx
+++ src/components/backgroundAnimation/GlowingBackground.jsx
@@ -7,9 +7,9 @@ export default function GlowingBackground({ zIndex = -1 }) {
         position: 'absolute',
         inset: 0,
         zIndex: zIndex, // default to -1 so glows behind stars (z=0)
-        background: 'radial-gradient(circle, rgba(159,87,255,0.7) 100%, rgba(59,30,76,0.2) 70%, rgba(34,14,54,0.2) 100%)',
+        background: 'radial-gradient(circle, rgba(159,87,255,0.7) 40%, rgba(59,30,76,0.2) 50%, rgba(34,14,54,0.2) 100%)',
         borderRadius: '90%',
-        filter: 'blur(60px)',
+        filter: 'blur(70px)',
         pointerEvents: 'none',
       }}
     />
