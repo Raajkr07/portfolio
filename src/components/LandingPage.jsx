fb6665c04cc183837ca932210fccc759dae11667
RAJ KUMAR <rk8210032@gmail.com>
2025-09-18 23:23:05 +0530

    new component tahs added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,16 @@ import { useInView } from "react-intersection-observer";
 import Header from './header/Header';
 import Footer from '../components/footer/footer';
 import HeroSection from './landingPageSection/HeroSection';
-import FielsExperties from '../components/landingPageSection/FieldSection';
+import FieldsExperties from '../components/landingPageSection/FieldSection';
 import TechnicalSkills from '../components/landingPageSection/TechnicalSkills';
 import Projects from '../components/landingPageSection/ProjectSection';
 import ConcactUs from '../components/landingPageSection/ContatcUsSection';
 import LineImage from '../components/landingPageSection/images/ImageSection';
 import FloatingSocialIcons from '../components/landingPageSection/FloatingSocialLinks';
 import SectionLinks from '../components/links/SectionLinks'
+import MagicBento from './reactBits/MagicBento'
+import SectionLink from './reactBits/SectionLinks';
+import logo from '../assets/logos/infinite';
 
 const LandingPage = () => {
 
@@ -23,11 +26,41 @@ const LandingPage = () => {
       <main className="flex-grow pt-[70px] relative">
         <HeroSection innerRef={heroRef} id="hero-section" />
         <FloatingSocialIcons inView={inView} />
-        <FielsExperties id="fields-section" />
+        <FieldsExperties id="fields-section" />
+        {/* <MagicBento
+          textAutoHide={true}
+          enableStars={true}
+          enableSpotlight={true}
+          enableBorderGlow={true}
+          enableTilt={true}
+          enableMagnetism={true}
+          clickEffect={true}
+          spotlightRadius={300}
+          particleCount={12}
+          glowColor="132, 0, 255"
+        /> */}
         <TechnicalSkills id="skills-section" />
         <Projects id="projects-section" />
         <ConcactUs id="contact-section" />
         <LineImage />
+        {/* <SectionLink
+          inView={inView}
+          logo={logo}
+          logoAlt="Company Logo"
+          items={[
+            { label: 'Home', href: '/' },
+            { label: 'About', href: '/about' },
+            { label: 'Services', href: '/services' },
+            { label: 'Contact', href: '/contact' }
+          ]}
+          activeHref="/"
+          className="custom-nav"
+          ease="power2.easeOut"
+          baseColor="#000000"
+          pillColor="#ffffff"
+          hoveredPillTextColor="#ffffff"
+          pillTextColor="#000000"
+        /> */}
         <SectionLinks inView={inView}/>
       </main>
       <Footer />
